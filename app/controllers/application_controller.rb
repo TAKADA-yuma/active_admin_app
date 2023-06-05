75bcf5a1fde1b69da72c338666d0c8c54d63f05a
TAKADA-yuma <89758513+TAKADA-yuma@users.noreply.github.com>
2023-06-05 21:01:58 +0900

    管理者画面にBasic認証の追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,8 @@
 class ApplicationController < ActionController::Base
+    
+    def basic_admin
+        authenticate_or_request_with_http_basic do |username, password|
+            username == ENV["admin"] && password == ENV["2222"]
+        end
+    end
 end
